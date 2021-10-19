ed307d0990ecfd6f6e662e972dbb93083118aaa9
Thomas Athey <tom.l.athey@gmail.com>
2021-10-19 10:07:13 -0400

    progress file

--- experiments/bil/bil_to_opennd.py
+++ experiments/bil/bil_to_opennd.py
@@ -4,12 +4,24 @@ from skimage import io
 from joblib import Parallel, delayed
 import multiprocessing
 import os
+import warnings
 
 ncpu = 12
 precomputed_path = "s3://open-neurodata/brainlit/bil1"
 files_dir = '/data/tathey1/bil/files_bay/'
+progress_file = "/home/tathey1/progress.txt"
 vol = CloudVolume(precomputed_path)
 print(f"cv shape: {vol.shape}")
+
+with open(progress_file) as f:
+    for line in f:
+        pass
+    last_line = line
+z_progress = int(last_line)
+
+print(f"Starting at {}")
+
+warnings.filterwarnings("ignore")
 
 def upload_z(z):
     filepath = '/data/tathey1/bil/files_bay/' + str(z) + '.tif'
@@ -24,8 +36,14 @@ def upload_z(z):
     im = io.imread(filepath)
     vol[:,:,z] = im.T
 
-for z_start in range(vol.shape[2], ncpu):
+for z_start in range(z_progress, vol.shape[2], ncpu):
+
+    with open(progress_file, 'a') as f:
+        f.write('\n')
+        f.write(f'{z_start}')
+
     Parallel(n_jobs=ncpu)(delayed(upload_z)(z) for z in range(z_start, z_start+ncpu))
-    
+
     for f in os.listdir(files_dir):
         os.remove(os.path.join(files_dir, f))
+
