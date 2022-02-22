4199d0f44cb79f11f802717892a1d9d8a018059a
Thomas Athey <tom.l.athey@gmail.com>
2022-02-22 14:04:36 -0500

    new parallel

--- experiments/bil/bil_script.py
+++ experiments/bil/bil_script.py
@@ -52,11 +52,12 @@ def viterbrain():
         chunk_size=[5,1000,1000],
         soma_coords=[],
         parallel=16,
+        prob_path="/data/tathey1/bil/image_probs.zarr"
     )
 
-    sg.predict(
-        data_bin="/data/tathey1/bil/files_bay/"
-    )
+    # sg.predict(
+    #     data_bin="/data/tathey1/bil/files_bay/"
+    # )
     sg.compute_frags()
     sg.compute_soma_lbls()
     sg.compute_image_tiered()
