b139d818ba994308ca5fee4637dd2fe1bb6d05c5
Thomas Athey <tom.l.athey@gmail.com>
2022-03-03 13:43:38 -0500

    diff files

--- experiments/bil/bil_script.py
+++ experiments/bil/bil_script.py
@@ -60,6 +60,9 @@ def viterbrain():
     #     data_bin="/data/tathey1/bil/files_bay/",
     #     pos_class = 0
     # )
+
+    sg._split_frags_thread([0, 8000, 4500], [5, 8500, 5000], data_bin="/data/tathey1/bil/files_bay/")
+    raise ValueError()
     sg.compute_frags(data_bin="/data/tathey1/bil/files_bay/")
     sg.compute_soma_lbls()
     sg.compute_image_tiered()
