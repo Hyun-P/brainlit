831350be0c6021f3556155abe56ca3a719c4135b
Thomas Athey <tom.l.athey@gmail.com>
2022-03-03 14:18:25 -0500

    diff files

--- brainlit/preprocessing/image_process.py
+++ brainlit/preprocessing/image_process.py
@@ -467,6 +467,7 @@ def split_frags_place_points(
     top = image_iterative[top_ind[0], top_ind[1], top_ind[2]]
 
     radius_vox = np.divide(radius_states, res).astype(int)
+    radius_vox = [np.amax([1, i]) for i in radius_vox]
 
     prev_tot = np.sum(image_iterative > threshold)
 
@@ -493,7 +494,6 @@ def split_frags_place_points(
                 np.amin([image_iterative.shape[1], top_ind[1] + radius_vox[1]]),
                 np.amin([image_iterative.shape[2], top_ind[2] + radius_vox[2]]),
             ]
-            print(f"{l_bd} to {u_bd}")
             image_iterative[l_bd[0] : u_bd[0], l_bd[1] : u_bd[1], l_bd[2] : u_bd[2]] = 0
 
             top_ind = np.unravel_index(
